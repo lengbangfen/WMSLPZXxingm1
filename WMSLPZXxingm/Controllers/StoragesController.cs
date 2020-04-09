3e0e27fbe48e43d8d5cf7d87e7a5aa5ffb8a008f
lbf992 <1960771864@qq.com>
2020-04-09 20:39:43 +0800

    2020-4-9

---
+++ WMSLPZXxingm/Controllers/StoragesController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WMSLPZXxingm.Controllers
+{
+    public class StoragesController : Controller
+    {
+        // GET: Storages
+        public ActionResult Index()
+        {
+            return View();
+        }
+        //登录页面
+        public ActionResult loginpage()
+        {
+            return View();
+        }
+        public ActionResult Welcome()
+        {
+            return View();
+        }
+    }
+}
